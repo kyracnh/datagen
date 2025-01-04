2429379f6fd9071fc871a263ddd1ebea41760636
Ahmed Anmazir <kyracnh>
2025-01-04 22:23:04 +0100

    First google search api integration scrapper + UI

--- datagen/scraper/views.py
+++ datagen/scraper/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from googleapiclient.discovery import build
+from urllib.parse import urlparse
 
 def search_google(request):
     results = []
+    competitor_count = 0
     query = ""
+    
     if request.method == "POST":
         query = request.POST.get("query")
         api_key = "AIzaSyB9oLSWryW7NbsxL86hi5Q3dcGdU6X24wg"
@@ -14,6 +17,14 @@ def search_google(request):
         res = service.cse().list(q=query, cx=cse_id).execute()
         
         if "items" in res:
-            results = res["items"]
+            results = [
+                {"name": item["title"], "link": item["link"]}
+                for item in res["items"][:5]
+            ]
+            competitor_count = len(results)  # Number of competitors found
     
-    return render(request, "scraper/results.html", {"query": query, "results": results})
+    return render(request, "scraper/results.html", {
+        "query": query,
+        "results": results,
+        "competitor_count": competitor_count,
+    })
